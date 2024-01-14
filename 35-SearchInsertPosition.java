4f90f9d2d00cb986c07306f6d571021d488f1cb1
Prudhvi <54917815+zeronimo09@users.noreply.github.com>
2024-01-14 10:46:37 -0800

    Added notes

--- 35-SearchInsertPosition.java
+++ 35-SearchInsertPosition.java
@@ -12,6 +12,8 @@ class Solution {
         }
     }
 
+Notes: In linear Search, nums[i]>= target will check if the target is found or it should be inserted if its not found in the array 
+
 
 // Binary Search
 class Solution {
@@ -31,3 +33,14 @@ class Solution {
         return left;
        } 
     }
+
+Logic:
+1. we return left to avoid Index out of bounds for left and right pointers when only one integer is given in nums array and we need to find an element to put after or before its position.
+examples: 
+nums = [2], target = 3
+nums = [2], target = 1
+
+Why: 
+1. If we search for whole array using linear search then the Time complexity is O(n).
+2. Since we are given a sorted array we should choose binary search for searching. Time complexity is O(logn). Because Binary search will cancel out half the array when traversing.
+    
