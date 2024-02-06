5e523b8c144256c5c0fde6e8d59ca6f967bb5d67
Prudhvi <54917815+zeronimo09@users.noreply.github.com>
2024-02-05 22:07:48 -0800

    Update 101-SymmetricTree.java

--- 101-SymmetricTree.java
+++ 101-SymmetricTree.java
@@ -1,3 +1,5 @@
+Java: Using Recursion
+//Creating a Mirror of the tree and the using recursion to compare the left & right SubTrees
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         return recursion(root, root);
@@ -8,3 +10,17 @@ class Solution {
         return (t1.val == t2.val) && recursion(t1.left, t2.right) && recursion(t1.right,t2.left);
     }
 }
+
+
+Python: Using Recursion
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.recursion(root, root)
+    
+    def recursion(self, t1: TreeNode, t2: TreeNode) -> bool:
+        if t1 is None and t2 is None:
+            return True            
+        if t1 is None or t2 is None:
+            return False    
+        return (t1.val == t2.val) and self.recursion(t1.left, t2.right) and self.recursion(t1.right, t2.left)
+
