63484a409442d656f2f52979fd113ad54a8ddd37
Prudhvi <54917815+zeronimo09@users.noreply.github.com>
2024-02-07 14:44:24 -0800

    Create 104-MaximumDepthofBinaryTree.py

---
+++ 104-MaximumDepthofBinaryTree.py
@@ -0,0 +1,7 @@
+Python: Using Recursion
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
