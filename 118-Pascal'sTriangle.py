ef01330cd004e4a0cb76ead1ddf84a0e4dbd39ce
Prudhvi <54917815+zeronimo09@users.noreply.github.com>
2024-03-04 13:12:38 -0800

    Update 118-Pascal'sTriangle.py

--- 118-Pascal'sTriangle.py
+++ 118-Pascal'sTriangle.py
@@ -10,3 +10,5 @@ class Solution:
                 row.append(temp[j]+temp[j+1])
             res.append(row)
         return res
+
+Notes: In Python, negative indices are used to access elements from the end of a list. So -1 would get us the last element.
